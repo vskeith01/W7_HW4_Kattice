f73e3a660a2bd3a1b6f960a45a8de41cfb2767bd
vskeith <111769909+vskeith01@users.noreply.github.com>
2022-10-12 01:56:00 +0000

    first function

--- .ccls-cache/@home@runner@W7HW4Kattice/main.cpp
+++ .ccls-cache/@home@runner@W7HW4Kattice/main.cpp
@@ -6,5 +6,29 @@ void fillArray(int list[], int listSize);
 int indexSmallestElement(const int list[], int listSize);
 
 int main() {
-  
+  int n;
+  cin >> n;
+
+  int list[n];
+
+  fillArray(list, n);
+
+  cout << indexSmallestElement(list, n);
+
+  return 0;
+}
+
+
+void fillArray(int list[], int listSize) {
+
+  cout << "Enter " << listSize << " integers: "; // comment out for kattis
+
+  int index;
+
+  for (index = 0; index < listSize; index++) {
+    cin >> list[index];
   }
+
+  return;
+}
+
